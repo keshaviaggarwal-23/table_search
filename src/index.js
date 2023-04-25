f5c41cd67df9ad897e7ff0460517cc3295005dba
Keshavi Aggarwal <keshavi.aggarwal@Geminisolutions.com>
2023-04-25 10:51:37 +0530

    dev tools use

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {Provider} from 'react-redux';
 import allReducers from './reducers'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const store = createStore(allReducers)
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() )
 root.render(
   <React.StrictMode>
     <Provider store = {store}><App /></Provider>
